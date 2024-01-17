ab15df97f5c4d55473f1ba58d0a62f3d8190f83e
colinbousige <colin.bousige@gmail.com>
2024-01-17 18:34:42 +0100

    added full example fits

--- 4-nest_map.R
+++ 4-nest_map.R
@@ -57,3 +57,34 @@ distribs$A <- c(distribs$A, NA)
 library(broom)
 measurements %>% 
     ___
+
+
+
+
+
+# # # # # # # # # # # # # # # # # # # 
+# Full example
+# # # # # # # # # # # # # # # # # # # 
+
+# Let's read all the files in the `Data/fits` folder. They contain distributions of observed carbon nanotube sizes as a function of temperature and time.
+
+flist <- list.files(___)
+
+# In a single pipe workflow:
+# - read all the files in the `Data/fits` folder. 
+# - nest the tibble by filename
+# - The files contain distributions of observed carbon nanotube sizes as a function of temperature and time: retrieve these informations from the file names. 
+# - Get the average size and standard deviation for each file
+# - nest the data by temperature
+# - then perform the linear fit of the average size as a function of time for each temperature. 
+
+
+data <- read_csv(___, id=___, col_name='L') %>% 
+    ___
+
+
+# Plot the result of the fits on the same graph as the data.
+
+
+
+# Now plot the slope as a function of temperature. It follows an Arrhenius law (a*exp(-Ea/T)). Fit this law to the data and extract the activation energy. Plot the fit on the same graph as the data.
