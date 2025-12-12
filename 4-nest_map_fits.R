ff4d11b0ba9a86062bf58cbad1a118df5b72a00e
colinbousige <colin.bousige@gmail.com>
2025-12-12 10:31:56 +0100

    added fit in 4

--- 4-nest_map_fits.R
+++ 4-nest_map_fits.R
@@ -4,15 +4,97 @@ library(readxl)
 library(glue)
 library(broom)
 library(latex2exp)
-theme_set(theme_bw()+
-    theme(text = element_text(size = 18, color = "black"),
-          panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
-          panel.background = element_rect(fill = "transparent", color = NA),
-          plot.background = element_rect(fill="transparent", color=NA),
-          legend.background = element_rect(fill = "transparent", color = NA),
-          strip.background = element_rect(fill = "transparent", color = NA),
-          strip.text = element_text(face = "bold"),
-          strip.text.y = element_text(angle=0)))
+theme_set(
+    theme_bw() +
+        theme(
+            text = element_text(size = 18, color = "black"),
+            panel.border = element_rect(
+                color = "black",
+                fill = NA,
+                linewidth = 1
+            ),
+            panel.background = element_rect(fill = "transparent", color = NA),
+            plot.background = element_rect(fill = "transparent", color = NA),
+            legend.background = element_rect(fill = "transparent", color = NA),
+            strip.background = element_rect(fill = "transparent", color = NA),
+            strip.text = element_text(face = "bold"),
+            strip.text.y = element_text(angle = 0)
+        )
+)
+
+
+# # # # # # # # # # # # # # # # # # #
+# Linear fitting
+# # # # # # # # # # # # # # # # # # #
+
+# Read the data in `Data/fit1.csv` into a data.frame called `d1`.
+# Plot the data, and make the appropriate fit using `lm()`.
+# Overlay the result of the fit as a red line
+# Hint: look at `predict()`
+
+d1 <- ___
+P1 <- d1 |>
+    ggplot(aes(___)) +
+    geom____()
+P1
+fit <- lm(___)
+P1 + geom_line(aes(y = ___), ___)
+
+# Read the data in `Data/fit2.csv` into a data.frame called `d2`.
+# Plot the data, and make the appropriate fit using `lm()`.
+# Overlay the result of the fit as a red line
+# Hint: look at `poly()`. Polynomial fits are actually linear fits!
+
+d2 <- ___
+
+
+# # # # # # # # # # # # # # # # # # #
+# Nonlinear fitting
+# # # # # # # # # # # # # # # # # # #
+
+# Read `Data/rubis_01.txt` into a data.frame called `d` with the column names `w` and `int`.
+# Hint: look at the options of `read.table()`
+d <- ___
+
+# Plot the intensity as a function of wavenumber using `ggplot()`.
+P <- ggplot(___) + 
+    ___
+P
+
+# The two peaks are Lorentzian peaks, of the form:
+# a/(1 + ((w-w0)/b)^2),
+# where a, b, and x0 are the parameters to be fitted.
+# Create a function `lor()` that defines a Lorentzian peak.
+lor <- function(w, a, w0, b) {
+    ___
+}
+
+# To find the starting values for the parameters, we will add as a line to the plot the sum of two Lorentzian peaks with guessed parameters so that the starting point is not too far from the data to fit.
+initial_guess <- tibble(
+    w = ___,
+    peak1 = ___,
+    peak2 = ___,
+    int = peak1 + peak2
+)
+P +
+    geom_line(___)
+
+# Perform the fit using `nls()`, then add the resulting fit to the plot as a red line.
+# Hint: look at `predict()`
+fit <- nls(
+    ___, # what is the data.frame with our data
+    ___, # what is the formula to fit? `~` means `as a function of`
+    start = list(___) # initial values for the parameters to fit
+) 
+P +
+    geom_line(___) + # initial guess
+    geom_line(___) + # nls fit
+    labs(___) # add labels to the axes
+
+
+# # # # # # # # # # # # # # # # # #
+# Fitting multiple datasets at once
+# # # # # # # # # # # # # # # # # #
 
 # Find list of all files in the Data directory with the pattern "sample" in their name
 flist <- list.files(___
--- 4-nest_map_fits_solution.R
+++ 4-nest_map_fits_solution.R
@@ -22,6 +22,98 @@ theme_set(
         )
 )
 
+
+# # # # # # # # # # # # # # # # # # #
+# Linear fitting
+# # # # # # # # # # # # # # # # # # #
+
+# Read the data in `Data/fit1.csv` into a data.frame called `d1`.
+# Plot the data, and make the appropriate fit using `lm()`.
+# Overlay the result of the fit as a red line
+# Hint: look at `predict()`
+
+d1 <- read_csv("Data/fit1.csv")
+P1 <- d1 |>
+    ggplot(aes(x = x, y = y)) +
+    geom_point()
+P1
+fit <- lm(y ~ x, data = d1)
+P1 + geom_line(aes(y = predict(fit)), color = 'red', linewidth = 1)
+
+# Read the data in `Data/fit2.csv` into a data.frame called `d2`.
+# Plot the data, and make the appropriate fit using `lm()`.
+# Overlay the result of the fit as a red line
+# Hint: look at `poly()`. Polynomial fits are actually linear fits!
+
+d2 <- read_csv("Data/fit2.csv")
+P2 <- d2 |>
+    ggplot(aes(x = x, y = y)) +
+    geom_point()
+P2
+fit <- lm(y ~ poly(x, 2), data = d2)
+P2 + geom_line(aes(y = predict(fit)), color = 'red', linewidth = 1)
+
+
+# # # # # # # # # # # # # # # # # # #
+# Nonlinear fitting
+# # # # # # # # # # # # # # # # # # #
+
+# Read `Data/rubis_01.txt` into a data.frame called `d` with the column names `w` and `int`.
+# Hint: look at the options of `read.table()`
+d <- read_table("Data/rubis_01.txt", col_names = c('w', 'int'))
+
+# Plot the intensity as a function of wavenumber using `ggplot()`.
+P <- ggplot(d, aes(x = w, y = int)) +
+    geom_point()
+P
+
+# The two peaks are Lorentzian peaks, of the form:
+# a/(1 + ((w-w0)/b)^2),
+# where a, b, and x0 are the parameters to be fitted.
+# Create a function `lor()` that defines a Lorentzian peak.
+lor <- function(w, a, w0, b) {
+    a / (1 + ((w - w0) / b)^2)
+}
+
+# To find the starting values for the parameters, we will add as a line to the plot the sum of two Lorentzian peaks with guessed parameters so that the starting point is not too far from the data to fit.
+initial_guess <- tibble(
+    w = d$w,
+    peak1 = lor(d$w, 700, 3170, 10),
+    peak2 = lor(d$w, 500, 3210, 10),
+    int = peak1 + peak2
+)
+P +
+    geom_line(data = initial_guess, color = 'red', linewidth = 1)
+
+# Perform the fit using `nls()`, then add the resulting fit to the plot as a red line.
+# Hint: look at `predict()`
+fit <- nls(
+    data = d, # what is the data.frame with our data
+    int ~ lor(w, a1, w01, b1) + lor(w, a2, w02, b2), # what is the formula to fit? `~` means `as a function of`
+    start = list(a1 = 700, w01 = 3170, b1 = 10, a2 = 500, w02 = 3210, b2 = 10)
+) # initial values for the parameters to fit
+P +
+    geom_line(
+        data = initial_guess,
+        aes(color = 'Initial guess'),
+        linewidth = 1
+    ) +
+    geom_line(aes(y = predict(fit), color = 'NLS fit'), linewidth = 1) +
+    scale_color_manual(
+        name = NULL,
+        values = c('Initial guess' = 'red', 'NLS fit' = 'royalblue')
+    ) +
+    labs(
+        title = "Nonlinear fit of two Lorentzian peaks",
+        y = "Intensity [a.u.]",
+        x = "Wavenumber [1/cm]"
+    )
+
+
+# # # # # # # # # # # # # # # # # #
+# Fitting multiple datasets at once
+# # # # # # # # # # # # # # # # # #
+
 # Find list of all files in the Data directory with the pattern "sample" in their name
 flist <- list.files(path = "Data", pattern = "sample", full.names = TRUE)
 # Read the files in the list and store them in a list of dataframes
